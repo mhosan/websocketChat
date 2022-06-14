5149f16c192ea004df6fd568cee88ef30762cab9
marcelo <mhosan@gmail.com>
2022-06-13 23:04:39 -0300

    ajustando

--- router/index.js
+++ router/index.js
@@ -1,57 +1,63 @@
 const { Router } = require('express');
 const router = Router();
-const Contenedor = require('../claseContenedor');
 const path = require('path');
 
-const productosPath = path.join(__dirname, 'productos.json');
-const contenedor = new Contenedor(productosPath);
+const productos = [];
 
 router.get('/productos', (req, res) => {
     console.log('GET /productos respondiendo');
-    contenedor.getAll()
-        .then(data => {
-            res.json(data);
-        }
-        ).catch(err => {
-            res.send(err);
-        }
-        );
-});
+    res.json(productos);
+}
+);
 
-router.post('/users', (req, res) => {
+router.get('/productos/:id', (req, res) => {
+    console.log('GET /productos por id respondiendo');
+    const id = req.params.id;
+    const producto = productos.find(p => p.id === Number(id));
+    res.json(producto);
+}
+);
+
+router.post('/productos', (req, res) => {
     console.log('POST /api/users respondiendo');
     if (Object.entries(req.body).length === 0) {
         res.status(400).json({ msg: 'No se recibieron datos' });
     } else {
-        res.json({ datos: req.body });
+        const { title, author, price, thumbnail } = req.body;
+        id = productos.length + 1;
+        productos.push({ id, title, author, price, thumbnail });
+        res.json({ id, title, author, price, thumbnail });
     }
-
 });
-const personas =[];
-const mascotas = [];
 
-router.get('/personas', (req, res) => {
-    res.json(personas);
-}
-);
-router.get('/mascotas', (req, res) => {
-    res.json(mascotas);
-}
-);
-
-router.post('/personas', (req, res) => {
-    const {nombre, apellido, edad} = req.body;
-    personas.push({nombre, apellido, edad});
-    res.sendStatus(201);
-}
-);
-
-router.post('/mascotas', (req, res) => {
-    const {nombre, raza, edad} = req.body;
-    mascotas.push({nombre, raza, edad});
-    res.sendStatus(201);
+router.put('/productos/:id', (req, res) => {
+    console.log('PUT /productos/:id respondiendo');
+    const id = req.params.id;
+    const producto = productos.find(p => p.id === Number(id));
+    if (producto) {
+        const { title, author, price, thumbnail } = req.body;
+        producto.title = title;
+        producto.author = author;
+        producto.price = price;
+        producto.thumbnail = thumbnail;
+        res.json(producto);
+    } else {
+        res.status(404).json({ msg: 'Producto no encontrado' });
+    }
 }
 );
 
+router.delete('/productos/:id', (req, res) => {
+    console.log('DELETE /productos/:id respondiendo');
+    const id = req.params.id;
+    const producto = productos.find(p => p.id === Number(id));
+    if (producto) {
+        productos.splice(productos.indexOf(producto), 1);
+        res.json(producto);
+    } else {
+        res.status(404).json({ msg: 'Producto no encontrado' });
+    }
+}
+);
 
 module.exports = router;
