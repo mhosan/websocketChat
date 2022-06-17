27914ea3fff5c9ca01e88c3945660f599040b4ef
marcelo <mhosan@gmail.com>
2022-06-17 19:37:53 -0300

    preparando desafio 5

--- desafio05/index.js
+++ desafio05/index.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 8080;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(express.static("public"));
+app.use('/api', router);
+
 const server = app.listen(port, () => {
     console.log(`Escuchando en el puerto ${port}`);
 }
 );
 server.on("error", error => { console.log(error) });
-
-app.use(express.static("public"));
-app.use('/api', router);
 
 
 
